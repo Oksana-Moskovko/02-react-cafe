df01e2a4c5f53979ce12bd26a01dff1ab6c0f129
Oksana <omoskovko98@gmail.com>
2025-06-28 21:59:23 +0300

    did the task

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import './index.css'
+import App from './components/App'
+// import CafeInfo from './CafeInfo.tsx'
+
+// Нормалізація стилів
+import "modern-normalize";
+// Глобальні стилі (додатково)
+import "./global.css";
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
